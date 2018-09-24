67c8f3e76aef6b755f2e2821c7ec5ce317730f6e
Molly McCarron <mccarronmollye@gmail.com>
2018-09-24 00:26:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,4 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  array.unshift('element')
-  console.log(array)
 }
