c079d500e7a7ad07905e9a5ae0c78c6f4f5edd77
Molly McCarron <mccarronmollye@gmail.com>
2018-09-24 00:10:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 //var anothaOne = ["crunch"]
-addElementToBeginningOfArray(array, element) {
-  var element = "crunch"
-  array.unshift(element)
-}
+//addElementToBeginningOfArray(array, element) {
+  //var element = "crunch"
+  //array.unshift(element)
+//}
 //destructivelyAddElementToBeginningOfArray (chocolateBars, "crunch") [
   //chocolateBars = ["crunch", ...chocolateBars]
   //console.log(chocolateBars)
